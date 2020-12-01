a3f5d11187a7166186dc2d121e48d21c20117249
vungtranvan <vungka321@gmail.com>
2020-12-02 01:38:44 +0700

    Create Product

--- eShopSolution.ViewModels/Catalog/Products/ProductCreateRequest.cs
+++ eShopSolution.ViewModels/Catalog/Products/ProductCreateRequest.cs
@@ -1,18 +1,29 @@
 ﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
 
 namespace eShopSolution.ViewModels.Catalog.Products
 {
     public class ProductCreateRequest
     {
         public decimal Price { get; set; }
+
         public decimal OriginalPrice { get; set; }
+
         public int Stock { get; set; }
+
+        [Required(ErrorMessage = "Bạn phải nhập tên sản phẩm")]
         public string Name { set; get; }
+
         public string Description { set; get; }
+
         public string Details { set; get; }
+
         public string SeoDescription { set; get; }
+
         public string SeoTitle { set; get; }
+
         public string SeoAlias { get; set; }
+
         public string LanguageId { set; get; }
 
         public IFormFile ThumbnailImage { get; set; }
